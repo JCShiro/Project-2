7b8c2aafa2d3c7849e84e925464494b37904799d
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2025-10-24 12:28:25 +0100

    added JS code for player
    added the code for the player, needs fixing as seekbar not working. Also need to convert all mp3 files to webm

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 console.log("This text means the javascript is working!!")
-
+//GLOBAL VARIABLES
+//get a reference for the play button
+const playButton = document.getElementById("pause-play-button");
+//get a reference for the skip back button
+const skipBackButton = document.getElementById("skip-back-button")
+//get a reference for the skip forward button
+const skipForwardButton = document.getElementById("skip-forward-button");
+//get a reference to the text elements
+const trackTime = document.getElementById("current-time");
+const totalTime = document.getElementById("total-time");
+//get a reference to the seek bar
+const seekBar = document.getElementById("Seek-bar");
+//audio object to manage loading and playback
+const myAudio = new Audio("audio/Open-Arms.webm")
+//store if the user is interacting with the bar
+let isSeeking = false;
+//LISTENERS
+//listen for an audio load event
+myAudio.onloadedmetadata = () => {
+    //makes sure track time is 0
+    trackTime.innerHTML = formatTime(0);
+    //set totalTime to the song duration
+    totalTime.innerHTML = formatTime(myAudio.duration);
+    //set the bar max to match the total time however round down as decimals don't work
+    seekBar.max = Math.floor(myAudio.duration);
+    //set the bar to 0
+    seekBar.value = 0;
+}
+//when the play button is clicked, play or pause the audio according to the state
+playButton.onclick = () =>{
+    if(myAudio.paused == true){
+        myAudio.play();
+    }else{
+        myAudio.pause();
+    }
+}
+//manage play - pause icon
+myAudio.onplay = () =>{
+    playButton.src = "images/pause.svg"
+}
+myAudio.onpause = () =>{
+    playButton.src = "images/play.svg"
+}
+//when time updates, update the text too
+myAudio.ontimeupdate = () =>{
+    trackTime.innerHTML = formatTime(myAudio.currentTime);
+    //update the seekbar when user not interacting with it
+    if(isSeeking == false){
+        seekBar.value = Math.floor(myAudio.currentTime);
+    }
+}
 
 /**
  * This formatTime function will format time from seonds to a readable time
@@ -9,6 +59,7 @@ console.log("This text means the javascript is working!!")
 function formatTime(secs) {
     let hours = Math.floor(secs / 3600);
     let minutes = Math.floor((secs - (hours * 3600)) / 60);
+    let seconds = Math.floor((secs - (hours * 3600)) - minutes * 60);
     if (hours < 10) {
         hours = "0" + hours;
     }
