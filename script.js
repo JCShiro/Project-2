8a6ce6ecc16cd399a691a7d989a8d234c5e11bae
JCShiro <jacob.shadbolt@student.norwichuni.ac.uk>
2025-11-04 11:05:55 +0000

    Fully working audio player
    Fully working, do documentation

--- script.js
+++ script.js
@@ -1,5 +1,64 @@
 console.log("This text means the javascript is working!!")
 //GLOBAL VARIABLES
+//ARRAY of all song data
+const songs = [
+    //Open arms
+    {
+        coverImage: "/images/song-covers/Troy_Saga.jpg",
+        songFile: "/audio/Open-Arms.mp3",
+        songName: "Open Arms"
+    },
+    //Remember Them
+    {
+        coverImage: "/images/song-covers/Cyclops_Saga.jpg",
+        songFile: "/audio/Remember-Them.mp3",
+        songName: "Remember Them"
+    },
+    //Ruthlessness
+    {
+        coverImage: "/images/song-covers/The_ocean_saga2.jpg",
+        songFile: "/audio/Ruthlessness.mp3",
+        songName: "Ruthlessness"
+    },
+    //Would'nt you like
+    {
+        coverImage: "/images/song-covers/The_Circe_Saga.jpg",
+        songFile: "/audio/Wouldn't-You-Like.mp3",
+        songName: "Would'nt you like"
+    },
+    //Monster
+    {
+        coverImage: "/images/song-covers/Underworld_Saga.jpg",
+        songFile: "/audio/Monster.mp3",
+        songName: "Monster"
+    },
+    //Scylla
+    {
+        coverImage: "/images/song-covers/Thunder_Saga.jpg",
+        songFile: "/audio/Scylla.mp3",
+        songName: "Scylla"
+    },
+    //Legendary
+    {
+        coverImage: "/images/song-covers/Wisdom_Saga.jpg",
+        songFile: "/audio/Legendary.mp3",
+        songName: "Legendary"
+    },
+    //Dangerous
+    {
+        coverImage: "/images/song-covers/Vengeance_Saga.jpg",
+        songFile: "/audio/Dangerous.mp3",
+        songName: "Dangerous"
+    },
+    //Odysseus
+    {
+        coverImage: "/images/song-covers/Ithaca_Saga.jpg",
+        songFile: "/audio/Odysseus.mp3",
+        songName: "Odysseus"
+    }
+]
+//sets the first song
+let songIndex = 0
 //get a reference for the play button
 const playButton = document.getElementById("pause-play-button");
 //get a reference for the skip back button
@@ -17,6 +76,10 @@ const seekBar = document.getElementById("seek-bar");
 let myAudio = new Audio("audio/Open-Arms.mp3");
 //store if the user is interacting with the bar
 let isSeeking = false;
+//get a reference for the current song image
+const currentSongImg = document.getElementById("current-song-img");
+//get a reference for the current song name
+const currentSongName = document.getElementById("song-name-text")
 
 //SONG SELECTORS
 //get a reference for open arms
@@ -38,20 +101,17 @@ const dangerousBtn = document.getElementById("dangerous");
 //get a reference for odysseus
 const odysseusBtn = document.getElementById("odysseus");
 
-//SWITCHES
-openArmsBtn.onclick = () =>{
-    myAudio.pause = true;
-    trackTime.innerHTML = formatTime(0);
-    myAudio = new Audio("/audio/Open-Arms.mp3")
-}
-
-rememberThemBtn.onclick = () =>{
-    myAudio.pause = true;
-    trackTime.innerHTML = formatTime(0);
-    myAudio = new Audio("/audio/Remember-Them.mp3");
-}
-
 //LISTENERS
+const trackBtns = document.getElementsByClassName("trackBtn");
+for (let i = 0; i < trackBtns.length; i++) {
+    // console.log(trackBtns[i]) test to make sure it works
+    trackBtns[i].onclick = (event) => {
+        // console.log(event.target.dataset.id)
+        songIndex = event.currentTarget.dataset.id
+        playSong();
+        playButton.src = "images/play.svg";
+    }
+}
 //listen for an audio load event
 myAudio.onloadedmetadata = () => {
     //makes sure track time is 0
@@ -66,73 +126,84 @@ myAudio.onloadedmetadata = () => {
     soundBar.value = 100;
 }
 //when the play button is clicked, play or pause the audio according to the state
-playButton.onclick = () =>{
-    if(myAudio.paused == true){
+playButton.onclick = () => {
+    if (myAudio.paused == true) {
         myAudio.play();
-    }else{
+    } else {
         myAudio.pause();
     }
 }
 //manage play - pause icon
-myAudio.onplay = () =>{
+myAudio.onplay = () => {
     playButton.src = "images/pause.svg"
 }
-myAudio.onpause = () =>{
+myAudio.onpause = () => {
     playButton.src = "images/play.svg"
 }
 //when time updates, update the text too
-myAudio.ontimeupdate = () =>{
+myAudio.ontimeupdate = () => {
     trackTime.innerHTML = formatTime(myAudio.currentTime);
     //update the seekbar when user not interacting with it
-    if(isSeeking == false){
+    if (isSeeking == false) {
         seekBar.value = Math.floor(myAudio.currentTime);
     }
 }
 //when user interacts with the seekbar
-seekBar.oninput = () =>{
+seekBar.oninput = () => {
     isSeeking = true
 }
 //on seekbar change, update time
-seekBar.onchange = () =>{
+seekBar.onchange = () => {
     myAudio.currentTime = seekBar.value
     //if the audio is playing, change after interaction
-    if(myAudio.paused == false){
+    if (myAudio.paused == false) {
         myAudio.play();
     }
     //set isSeeking to false
     isSeeking = false
 }
 
-soundBar.oninput = () =>{
-    if (myAudio){
+soundBar.oninput = () => {
+    if (myAudio) {
         myAudio.volume = soundBar.value / 100;
     }
-
-    volumeTracker = soundBar.value/100;
 }
 
-//ARRAY of all song data
-const songs = [
-    //Open arms
-    ["/images/song-covers/Troy_Saga.jpg","/audio/Open-Arms.mp3","Open Arms"],
-    //Remember Them
-    ["/images/song-covers/Cyclops_Saga.jpg","/audio/Remember-Them.mp3","Remember Them"],
-    //Ruthlessness
-    ["/images/song-covers/The_ocean_saga2.jpg","/audio/Ruthlessness.mp3","Ruthlessness"],
-    //Would'nt you like
-    ["/images/song-covers/The_Circe_Saga.jpg","/audio/Wouldn't-You-Like.mp3","Would'nt you like"],
-    //Monster
-    ["/images/song-covers/Underworld_Saga.jpg","/audio/Monster.mp3","Monster"],
-    //Scylla
-    ["/images/song-covers/Thunder_Saga.jpg","/audio/Scylla.mp3","Scylla"],
-    //Legendary
-    ["/images/song-covers/Wisdom_Saga.jpg","/audio/Legendary.mp3","Legendary"],
-    //Dangerous
-    ["/images/song-covers/Vengeance_Saga.jpg","/audio/Dangerous.mp3","Dangerous"],
-    //Odysseus
-    ["/images/song-covers/Ithica_Saga.jpg","/audio/Odysseus.mp3","Odysseus"],
-]
+skipForwardButton.onclick = () => {
+    if (songIndex < songs.length-1) {
+        songIndex++
+    } else {
+        songIndex = 0
+    }
+    console.log(songIndex);
+    playSong();
+    playButton.src = "images/play.svg"
+}
 
+skipBackButton.onclick = () =>{
+    if (songIndex < 1 ) {
+        songIndex = songs.length-1
+    }else {
+        songIndex--
+    }
+    console.log(songIndex);
+    playSong();
+    playButton.src = "images/play.svg"
+}
+
+//SWITCHES
+// openArmsBtn.onclick = () =>{
+//     myAudio.pause = true;
+//     trackTime.innerHTML = formatTime(0);
+//     myAudio = new Audio("/audio/Open-Arms.mp3");
+// }
+
+// rememberThemBtn.onclick = () =>{
+//     trackTime.innerHTML = formatTime(0);
+//     myAudio.pause = true;
+
+//     myAudio = new Audio("/audio/Remember-Them.mp3");
+// }
 
 
 
@@ -159,4 +230,11 @@ function formatTime(secs) {
     } else {
         return minutes + ":" + seconds;
     }
+}
+
+function playSong() {
+    let data = songs[songIndex];
+    myAudio.src = data.songFile;
+    currentSongImg.src = data.coverImage;
+    currentSongName.innerHTML = data.songName;
 }
